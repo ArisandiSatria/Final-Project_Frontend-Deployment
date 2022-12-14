c6fbe61c26c47932a762132fb58c07d86789db97
Arisandi <arisandisatria541@gmail.com>
2022-12-14 22:48:42 +0700

    Uploud ke Github

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Arisandi Satria Jeujanan" // TODO: replace this
+const studentId = "FE2680377" // TODO: replace this
+
+export { url, studentName, studentId }
