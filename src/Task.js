9923534023e642d9b99f8955ffb648fac9bf6186
Arisandi <arisandisatria541@gmail.com>
2022-12-14 23:12:31 +0700

    Add netlify URL

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "YOURDEPLOYURL" // TODO: replace this
+const url = "https://galeryapp.netlify.app/"; // TODO: replace this
 const studentName = "Arisandi Satria Jeujanan" // TODO: replace this
 const studentId = "FE2680377" // TODO: replace this
 
